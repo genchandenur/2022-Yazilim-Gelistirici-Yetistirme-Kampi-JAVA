4e31ed301fdc15d178e397a211b7de11a3856517
Handenur Genç <61150526+genchandenur@users.noreply.github.com>
2022-09-26 15:04:18 +0300

    Add files via upload

---
+++ java-dersi-hw2-2/javadersiInheritance/src/CustomerManager.java
@@ -0,0 +1,5 @@
+public class CustomerManager extends PersonManager{
+    // person manager'a extends ettiğimizde person managerda aynı metodlar oldugundan override oluyor
+    // burda artık list ve add operasyonlarını tekrar eklemeye gerek yok.
+
+}
---
+++ java-dersi-hw2-2/javadersiInheritance/src/EmployeeManager.java
@@ -0,0 +1,8 @@
+public class EmployeeManager extends PersonManager{
+    // burda da aynı şekilde person manager'a extend ettiğimizde person managerdaki operasyonları
+    // tekrar burda eklememize gerek yok
+    public void BestEmployee(){
+        System.out.println("Ayın elemanı getirildi.");
+
+    }
+}
